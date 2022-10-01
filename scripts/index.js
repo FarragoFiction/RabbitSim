007157d69ac0ac67bc7ada46c302d42a5f4bc2eb
JR <jadedResearcher@gmail.com>
2022-09-30 23:52:02 -0400

    probably doesn't work but it got the bones in place before lseeping time

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,11 @@
 //TODO: make sure all assets are kept locally so when i upload it still works
 
+
 /*
 
 people are watching a "lets play" of zampanio. 
 
-every ten seconds theres a flash of something wrong.
+every ten seconds theres a flash of an ai melted "BE KIND: REWIND"
 
 they're discussing the 'rumors' about what happens to people who play zampanio. joking that its okay, they're only WATCHING someone play it.
 
@@ -14,59 +15,79 @@ in the console, truth is crooning to you. come. join.
 
 dig a little deeper. what could it hurt?
 
+if you rewind, the chat rewinds as well. they notice. you've caught them in a loop. if the video itself loops, too.
+
 */
 
-window.onload = ()=>{
-    startSimulation();
-}
-
-const startSimulation = ()=>{
-    const video = document.querySelector('#player');
-    video.onplay = ()=>{
-        window.alert("playing");
-    }
-}
 
 
-const story = [
-    new Rando1(5,[new ChatLine(0,"first guy says something at time code 5"), new ChatLine(10, "testing ten seconds later, this might overlap, not sure how to handle")]),
-    new Rando2(15,[new ChatLine(0,"second guy speaks up after 10 seconds"), new ChatLine(10, "testing ten seconds later")]),
+/*
+    multiple ways we could do this. the easiest is "when your time code hits, add yourself with a lil sound";
 
+    but one thing that could be interesting is 'if the video is rewound, so too is time'. (could play with that)
+*/
+//yes yes, i'm bad for using global variables, so sue me, i'm going fast
+
+const stories = [
+    [
+        new Rando1(5, [new ChatLine(0, "first guy says something at time code 5"), new ChatLine(10, "testing ten seconds later, this might overlap, not sure how to handle")]),
+        new Rando2(15, [new ChatLine(0, "second guy speaks up after 10 seconds"), new ChatLine(10, "testing ten seconds later")]),
+    ],
+    [
+        new Rando1(5, [new ChatLine(0, "wait, isn't this different?")]),
+        new Rando2(15, [new ChatLine(0, "no i think it was always like this")]),
+    ],
 ]
 
+//each time you loop, tell a different story.
+let timesLooped = 0;
+let chatBox;
 
+let latestSeen = 0; //if the videos current time code is LESS than this, i need to throw away the whole chat and render up till what i've seen
 
-class ChatLine{
-    content = "";
-    offset = 0;//offset from when the first one goes.
-    constructor(offset,content){
-        this.offset = offset;
-        this.content = content;
-    }
+const getCurrentStory = () => {
+    return stories[timesLooped % stories.length]; //no matter how many times we've looped theres alwyas something
 }
 
-class ChatItem{
-    icon_src;
-    name;
-    targetTimecode; //NOT display time code. display time code is when it actually went on screen, local time
-    displayTimecode;
-    lines = []; //chat lines
-    constructor(name, icon_src, targetTimecode, lines){
-        this.name = name;
-        this.icon_src = icon_src;
-        this.targetTimecode = targetTimecode;
-        this.lines = lines;
+const simulateChat = (event) => {
+    const video = document.querySelector('#player');
+    if (video.currentTime > latestSeen) {
+        if (video.currentTime > latestSeen + 10) { //every ten seconds it calculates. (also every ten seconds the video prompts you to rewind)
+            lookForNextEvent(video.currentTime)
+        }
+    } else {
+        reconcillePast(video.currentTime);
     }
+    latestSeen = video.currentTime;
 }
 
-class Rando1 extends ChatItem{
-    constructor( targetTimecode, lines){
-        super("Rando1", "http://farragofiction.com/TwoGayJokes/Stories/MurderGameIcons/k.png", targetTimecode, lines);
+//time is moving forwards. 
+const lookForNextEvent = (time) => {
+    const story = getCurrentStory();
+    for(let line of story){
+        if(line.targetTimecode > latestSeen && line.targetTimecode > time){
+            line.renderSelf(chatBox)
+        }
     }
+
 }
 
-class Rando2 extends ChatItem{
-    constructor( targetTimecode, lines){
-        super("Rando2", "http://farragofiction.com/TwoGayJokes/Stories/MurderGameIcons/theshot.pngg", targetTimecode, lines);
-    }
+//time is wrong
+const reconcillePast = (time) => {
+
+}
+
+
+
+const startSimulation = () => {
+    const video = document.querySelector('#player');
+    chatBox = document.querySelector("#chat-box");
+    video.addEventListener('timeupdate', simulateChat);
+}
+
+
+
+
+window.onload = () => {
+    startSimulation();
 }
---
+++ scripts/objects.js
@@ -0,0 +1,96 @@
+
+
+class ChatLine{
+    content = "";
+    offset = 0;//offset from when you were asked to render.
+    constructor(offset,content){
+        this.offset = offset;
+        this.content = content;
+    }
+
+    //TODO only render self after x seconds have gone by since you were asked
+    renderSelf = (target)=>{
+        const p = createElementWithParentAndClass("p",target);
+        p.innerHTML = this.content;
+    }
+
+
+}
+
+class ChatItem{
+    icon_src;
+    name;
+    isMe = false;
+    targetTimecode; //NOT display time code. display time code is when it actually went on screen, local time
+    displayTimecode;
+    lines = []; //chat lines
+    constructor(name, icon_src, targetTimecode, lines){
+        this.name = name;
+        this.icon_src = icon_src;
+        this.targetTimecode = targetTimecode;
+        this.lines = lines;
+    }
+
+    renderSelf = (target)=>{
+        /*
+        <div class="your chat">
+          <img class="circle chat-icon"
+            src="http://farragofiction.com/TwoGayJokes/Stories/MurderGameIcons/theend.png" />
+          <div class="chat-header">
+
+            <div class="name">theEnd</div>
+            <div class="timestamp">12:03 am</div>
+          </div>
+          <div class="chat-content">
+
+            <p>...</p>
+            <p>:(</p>
+          </div>
+        </div>
+        */
+       const container = createElementWithParentAndClass("div",target, `${this.isMe? "my":"your"} chat`);
+       const img = createElementWithParentAndClass("img",container, "circle chat-icon");
+       img.src = this.icon_src;
+       const header = createElementWithParentAndClass("div",container, "chat-header");
+       const name = createElementWithParentAndClass("div",header, "name");
+       name.innerHTML = this.name;
+       const timestamp = createElementWithParentAndClass("div",header, "timestamp");
+       timestamp.innerHTML = `${new Date().toTimeString()}`;
+
+       const content = createElementWithParentAndClass("div",container, "chat-content");
+       for (let line of this.lines){
+        line.renderSelf(content);
+       }
+
+    }
+
+
+}
+
+class Rando1 extends ChatItem{
+    isMe = true;
+    constructor( targetTimecode, lines){
+        super("Rando1", "http://farragofiction.com/TwoGayJokes/Stories/MurderGameIcons/k.png", targetTimecode, lines);
+    }
+}
+
+class Rando2 extends ChatItem{
+    constructor( targetTimecode, lines){
+        super("Rando2", "http://farragofiction.com/TwoGayJokes/Stories/MurderGameIcons/theshot.pngg", targetTimecode, lines);
+    }
+}
+
+const createElementWithClass = (eleName, className) => {
+    const ele = document.createElement(eleName);
+    if (className) {
+        ele.className = className;
+    }
+    return ele;
+}
+
+const createElementWithParentAndClass = (eleName, parent, className) => {
+    const ele = createElementWithClass(eleName, className);
+    parent.append(ele);
+    return ele;
+}
+
