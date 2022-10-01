5c70c6fa28b1549a622de9902c5814edfb9cb31e
JR <jadedResearcher@gmail.com>
2022-09-30 22:51:14 -0400

    scrolling works

---
+++ scripts/index.js
@@ -0,0 +1,13 @@
+//TODO: make sure all assets are kept locally so when i upload it still works
+
+/*
+
+people are watching a "lets play" of zampanio. 
+
+every ten seconds theres a flash of something wrong.
+
+they're discussing the 'rumors' about what happens to people who play zampanio. joking that its okay, they're only WATCHING someone play it.
+
+actually did you here that they found the streamers comatose body in an alleyway somewhere?
+
+*/
