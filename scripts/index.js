432fb3f328b19bd0b8aab41c313d23cb5c958fd0
JR <jadedResearcher@gmail.com>
2022-10-01 20:59:51 -0400

    looping works and debugged, async not working right if time traveling

--- scripts/index.js
+++ scripts/index.js
@@ -29,7 +29,7 @@ if you rewind, the chat rewinds as well. they notice. you've caught them in a lo
 //yes yes, i'm bad for using global variables, so sue me, i'm going fast
 
 const stories = [
-    [
+    {video_src: "http://farragofiction.com/ZampanioHotlink/Films/shittyNorth.mp4",chat: [
         new Rando1(0, [new ChatLine(0, "first guy says something almost immediately"), new ChatLine(1, "testing 1 seconds later, this might overlap, not sure how to handle")]),
         new Rando1(5, [new ChatLine(0, "first guy says something at time code 5"), new ChatLine(1, "testing 1 seconds later, this might overlap, not sure how to handle")]),
         new Rando2(15, [new ChatLine(0, "second guy speaks up after 10 seconds"), new ChatLine(1, "testing 1 seconds later")]),
@@ -37,35 +37,37 @@ const stories = [
         new Rando2(20, [new ChatLine(0, "second guy responds quickly too"), new ChatLine(1, "testing 1 seconds later")]),
         new Rando1(65, [new ChatLine(0, "first guy is quiet for a while, then says something"), new ChatLine(10, "testing ten seconds later")]),
 
-    ],
-    [
+    ]},
+    {video_src: "http://farragofiction.com/ZampanioHotlink/Films/thirdEast.mp4",chat:[
         new Rando1(5, [new ChatLine(0, "wait, isn't this different?")]),
         new Rando2(15, [new ChatLine(0, "no i think it was always like this")]),
-    ]
+    ]}
 ]
 
 //each time you loop, tell a different story.
 let timesLooped = 0;
 let chatBox;
+let video;
 
 let latestSeen = 0; //if the videos current time code is LESS than this, i need to throw away the whole chat and render up till what i've seen
 let latestInteracted = -10;
 
 const getCurrentStory = () => {
-    console.log("JR NOTE: stories is", stories.length, "and times looped is ", timesLooped, "so i think thats index ",timesLooped %stories.length, "or is it",  )
+    //console.log("JR NOTE: stories is", stories.length, "and times looped is ", timesLooped, "so i think thats index ",timesLooped %stories.length, "or is it",  )
     return stories[ timesLooped %stories.length]; //no matter how many times we've looped theres alwyas something
 }
 
 const simulateChat = (event) => {
-    const video = document.querySelector('#player');
+     video = document.querySelector('#player');
 
-    if (video.currentTime > latestSeen) {
+    if (video.currentTime >= latestSeen) {
         if (video.currentTime > latestInteracted + 10) { //every ten seconds it calculates. (also every ten seconds the video prompts you to rewind)
             lookForNextEvent(video.currentTime)
             latestInteracted = video.currentTime;
 
         }
     } else {
+        console.log(`JR NOTE: i need to reconcile the past because ${video.currentTime } is smaller than ${latestSeen}.`)
         reconcillePast(video.currentTime);
         latestInteracted = video.currentTime;
 
@@ -78,12 +80,12 @@ const simulateChat = (event) => {
 const lookForNextEvent = (time) => {
     //console.log("JR NOTE: time i am looking for the next event against is", time);
     const story = getCurrentStory();
-    for(let line of story){
+    for(let line of story.chat){
         //console.log("JR NOTE: line is", line.targetTimecode, "is that bigger than the latestInteracted? ",latestInteracted,line.targetTimecode > latestInteracted, " is it bigger than the time? ",time,latestInteracted && time > line.targetTimecode );
 
         if(line.targetTimecode > latestInteracted && time > line.targetTimecode ){
             //console.log("JR NOTE: yes, so i am going to render")
-            line.renderSelf(chatBox)
+            line.renderSelf(chatBox,time)
         }
     }
 
@@ -91,12 +93,19 @@ const lookForNextEvent = (time) => {
 
 //time is wrong
 const reconcillePast = (time) => {
+    console.log("JR NOTE: reconcilling the past");
     timesLooped ++;
-    chatBox.innerHTML = "";
     const story = getCurrentStory();
-    for(let line of story){
-        if(line.targetTimecode > time){ //we no longer care that we're only rendering things that are in the future
-            line.renderSelf(chatBox)
+
+    const timeSave = video.currentTime;
+    video.src = story.video_src;
+    video.currentTime = timeSave;
+    chatBox.innerHTML = "";
+    for(let line of story.chat){
+        console.log("JR NOTE: current story is", story, "should i render bits of it?")
+        if(line.targetTimecode < time){ //we no longer care that we're only rendering things that are in the future
+            console.log("JR NOTE: rendering ",line)
+            line.renderSelf(chatBox,time)
         }
     }
 }
--- scripts/objects.js
+++ scripts/objects.js
@@ -1,39 +1,47 @@
 
 
-class ChatLine{
+class ChatLine {
     content = "";
     offset = 0;//offset from when you were asked to render.
-    constructor(offset,content){
+    constructor(offset, content) {
         this.offset = offset;
         this.content = content;
     }
 
-    renderSelf = (target)=>{
-        setTimeout(()=>{
-            const p = createElementWithParentAndClass("p",target);
+    //imeediate if we're jumping around in time
+    renderSelf = (target, immediate) => {
+        if (immediate) {
+            console.log("JR NOTE: immediate render")
+            const p = createElementWithParentAndClass("p", target);
             p.innerHTML = this.content;
-        }, this.offset*1000)
+        } else {
+            setTimeout(() => {
+                const p = createElementWithParentAndClass("p", target);
+                p.innerHTML = this.content;
+            }, this.offset * 1000)
+
+        }
 
     }
 
 
 }
 
-class ChatItem{
+class ChatItem {
     icon_src;
     name;
     isMe = false;
     targetTimecode; //NOT display time code. display time code is when it actually went on screen, local time
     displayTimecode;
     lines = []; //chat lines
-    constructor(name, icon_src, targetTimecode, lines){
+    constructor(name, icon_src, targetTimecode, lines) {
         this.name = name;
         this.icon_src = icon_src;
         this.targetTimecode = targetTimecode;
         this.lines = lines;
     }
 
-    renderSelf = (target)=>{
+    renderSelf = (target, timecode, immediate = false) => {
         /*
         <div class="your chat">
           <img class="circle chat-icon"
@@ -50,34 +58,34 @@ class ChatItem{
           </div>
         </div>
         */
-       const container = createElementWithParentAndClass("div",target, `${this.isMe? "my":"your"} chat`);
-       const img = createElementWithParentAndClass("img",container, "circle chat-icon");
-       img.src = this.icon_src;
-       const header = createElementWithParentAndClass("div",container, "chat-header");
-       const name = createElementWithParentAndClass("div",header, "name");
-       name.innerHTML = this.name;
-       const timestamp = createElementWithParentAndClass("div",header, "timestamp");
-       timestamp.innerHTML = ` ${new Date().toLocaleTimeString()}`;
+        const container = createElementWithParentAndClass("div", target, `${this.isMe ? "my" : "your"} chat`);
+        const img = createElementWithParentAndClass("img", container, "circle chat-icon");
+        img.src = this.icon_src;
+        const header = createElementWithParentAndClass("div", container, "chat-header");
+        const name = createElementWithParentAndClass("div", header, "name");
+        name.innerHTML = this.name;
+        const timestamp = createElementWithParentAndClass("div", header, "timestamp");
+        timestamp.innerHTML = ` ${new Date().toLocaleTimeString()}`;
 
-       const content = createElementWithParentAndClass("div",container, "chat-content");
-       for (let line of this.lines){
-        line.renderSelf(content);
-       }
+        const content = createElementWithParentAndClass("div", container, "chat-content");
+        for (let line of this.lines) {
+            line.renderSelf(content, immediate, timecode <this.targetTimecode + 10); //if i was supposed to render more than ten seconds ago, no async behavior plz (useful for fastforward and)
+        }
 
     }
 
 
 }
 
-class Rando1 extends ChatItem{
+class Rando1 extends ChatItem {
     isMe = true;
-    constructor( targetTimecode, lines){
+    constructor(targetTimecode, lines) {
         super("Rando1", "http://farragofiction.com/TwoGayJokes/Stories/MurderGameIcons/k.png", targetTimecode, lines);
     }
 }
 
-class Rando2 extends ChatItem{
-    constructor( targetTimecode, lines){
+class Rando2 extends ChatItem {
+    constructor(targetTimecode, lines) {
         super("Rando2", "http://farragofiction.com/TwoGayJokes/Stories/MurderGameIcons/theshot.png", targetTimecode, lines);
     }
 }
